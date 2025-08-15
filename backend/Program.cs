2ccc1961793ef634089b3dc0256b9c05f291df85
RohannNWU <25130757@mynwu.ac.za>
2025-08-15 13:56:10 +0200

    added CORS

--- backend/Program.cs
+++ backend/Program.cs
@@ -4,6 +4,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("https://wonderful-rock-091893c03.1.azurestaticapps.net", "http://localhost:3000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
+});
 
 var app = builder.Build();
 
@@ -17,5 +27,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+app.UseCors();
 
 app.Run();
