5523f253cd592067e9e0ae71b4fe7e1e9c05e85c
RohannNWU <25130757@mynwu.ac.za>
2025-08-16 17:16:50 +0200

    edit

--- backend/Program.cs
+++ backend/Program.cs
@@ -2,7 +2,6 @@ using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
