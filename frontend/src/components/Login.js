b0c08c00c88be70c34fb9bf98df08bb352f04f37
RohannNWU <25130757@mynwu.ac.za>
2025-08-17 20:03:50 +0200

    fixed a typo

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -17,7 +17,7 @@ function Login() {
     try {
       const API_BASE_URL = window.location.hostname === "localhost" ? "http://localhost:5280" : "https://pcp-backend.azurewebsites.net";
 
-      const response = await fetch('${API_BASE_URL}/api/Login', {
+      const response = await fetch(`${API_BASE_URL}/api/Login`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
