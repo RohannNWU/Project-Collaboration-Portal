a4e027b2d70bcd3afb834f5c9aeeb98892970699
RohannNWU <25130757@mynwu.ac.za>
2025-08-30 05:29:13 +0200

    typo

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './Login.css';
+import './styles.css';
 
 function Login() {
   const [username, setUsername] = useState('');
