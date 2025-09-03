f8913f7651c1237a20e775fe1718ba6b26b56e03
RohannNWU <25130757@mynwu.ac.za>
2025-09-03 22:23:43 +0200

    ne backend

---
+++ backend/pcp_backend/pcp_webapp/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class PcpWebappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'pcp_webapp'
---
+++ backend/pcp_backend/pcp_webapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib import messages
+
+# Create your views here.
+def login_request(request):
+    return render(request, 'login.html')
+
+def login_handler(request):
+    user = request.POST.get('email_address')
+    password = request.POST.get('user_password')
+    print("User: ", user)
+    print("Password: ", password)
+    if (user == 'admin@email.com' and password == 'password'):
+        return render(request, 'landingpage.html')
+    else:
+        messages.error(request, 'Invalid email and password combination')
+        return render(request, 'login.html')
