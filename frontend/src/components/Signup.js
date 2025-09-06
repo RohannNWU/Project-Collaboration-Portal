701f368c99e805ce4c9b60764b8e468261a66869
RohannNWU <25130757@mynwu.ac.za>
2025-09-06 16:02:22 +0200

    asd

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './Signup.module.css';
 
 const Signup = () => {
@@ -6,6 +7,7 @@ const Signup = () => {
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const [success, setSuccess] = useState('');
+  const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
