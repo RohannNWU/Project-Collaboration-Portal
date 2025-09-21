c4d27e8db1906671842abba709bc117f4b5e4162
Jacques <35317906@mynwu.ac.za>
2025-09-21 19:04:32 +0200

    Calendar moved to Dashboard (Still need to fix bugs)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
+// App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import Signup from './components/Signup';
-import Calendar from './components/Calendar';
 import NewProject from './components/NewProject';
 
 function App() {
@@ -13,8 +13,8 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/adduser" element={<Signup />} />
-          <Route path="/calendar" element={<Calendar />} />
           <Route path="/newProject" element={<NewProject />} />
+          {/* Calendar route removed - calendar is now integrated into Dashboard */}
         </Routes>
     </Router>
   );
